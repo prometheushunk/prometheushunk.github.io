b9d87547e4312b5317cf38376b8535a8dc0b87e3
Midhun-Angathil <midhun.angathil@gmail.com>
2026-02-28 17:10:53 +0000

    chore: commit workspace changes

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 window.PH_CONFIG = {
-    YT_API_KEY: "AIzaSyA..............................................T5Bw"
+    YT_API_KEY: "AIzaSyA..............................................T5Bw",
+    CHANNEL_ID: "UCkPIB4exd1He-GSjzAm2Nmg",
+    MAX_RESULTS: 3
 };
