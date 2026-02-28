2e114a8803f85809437ad87e6860890f28fb00b2
Midhun-Angathil <midhun.angathil@gmail.com>
2026-02-28 13:43:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// config.js provides: window.PH_YT_API_KEY
-const API_KEY = window.PH_YT_API_KEY;
-const CHANNEL_ID = "UCX5uOaNn-ORyRNPWV2VN_lQ";  // Prometheus Hunk channel ID
+const API_KEY = "AIzaSyA.................8T5Bw";
+const CHANNEL_ID = "UCX5uOaNn-ORyRNPWV2VN_lQ";
 const MAX_RESULTS = 3;
 
 async function loadVideos() {
-    const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULTS}`;
+    const url =
+        `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}` +
+        `&part=snippet,id&order=date&maxResults=${MAX_RESULTS}`;
 
     const res = await fetch(url);
     const data = await res.json();
@@ -16,17 +17,17 @@ async function loadVideos() {
         const videoId = item.id.videoId;
         if (!videoId) return;
 
-        const card = document.createElement("div");
-        card.className = "video-card";
+        const div = document.createElement("div");
+        div.className = "video-card";
 
-        card.innerHTML = `
+        div.innerHTML = `
             <iframe 
                 src="https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=1"
                 allow="autoplay; encrypted-media"
             ></iframe>
         `;
 
-        carousel.appendChild(card);
+        carousel.appendChild(div);
     });
 }
 
